1718b38c7c05751045dd2981702e5231a06a5853
todd <thickam@outlook.com>
2023-11-07 01:09:37 -0800

    added canvas to .html to test game integration

--- react_client/sprite_game/main.js
+++ react_client/sprite_game/main.js
@@ -1,64 +1,62 @@
-import './style.css'
-import {resources} from "./src/Resource.js";
-import {Sprite} from "./src/Sprite.js";
-import {Vector2} from "./src/Vector2.js";
-import {GameLoop} from "./src/GameLoop.js";
-import {Input} from "./src/Input.js";
-import {gridCells} from "./src/helpers/grid.js";
-import {GameObject} from "./src/GameObject.js";
-import {Hero} from "./src/objects/Hero/Hero.js";
-import {Camera} from "./src/Camera.js";
-import {Rod} from "./src/objects/Rod/Rod.js";
-import {Inventory} from "./src/objects/Inventory/Inventory.js";
+import "./style.css";
+import { resources } from "./src/Resource.js";
+import { Sprite } from "./src/Sprite.js";
+import { Vector2 } from "./src/Vector2.js";
+import { GameLoop } from "./src/GameLoop.js";
+import { Input } from "./src/Input.js";
+import { gridCells } from "./src/helpers/grid.js";
+import { GameObject } from "./src/GameObject.js";
+import { Hero } from "./src/objects/Hero/Hero.js";
+import { Camera } from "./src/Camera.js";
+import { Rod } from "./src/objects/Rod/Rod.js";
+import { Inventory } from "./src/objects/Inventory/Inventory.js";
 
 // Grabbing the canvas to draw to
+
 const canvas = document.querySelector("#game-canvas");
 const ctx = canvas.getContext("2d");
 
 // Establish the root scene
 const mainScene = new GameObject({
-  position: new Vector2(0,0)
-})
+  position: new Vector2(0, 0),
+});
 
 // Build up the scene by adding a sky, ground, and hero
 const skySprite = new Sprite({
   resource: resources.images.sky,
-  frameSize: new Vector2(320, 180)
-})
+  frameSize: new Vector2(320, 180),
+});
 
 const groundSprite = new Sprite({
   resource: resources.images.ground,
-  frameSize: new Vector2(320, 180)
-})
+  frameSize: new Vector2(320, 180),
+});
 mainScene.addChild(groundSprite);
 
-const hero = new Hero(gridCells(6), gridCells(5))
+const hero = new Hero(gridCells(6), gridCells(5));
 mainScene.addChild(hero);
 
-const camera = new Camera()
+const camera = new Camera();
 mainScene.addChild(camera);
 
-const rod = new Rod(gridCells(7), gridCells(6))
+const rod = new Rod(gridCells(7), gridCells(6));
 mainScene.addChild(rod);
 
 const inventory = new Inventory();
 
-
 // Add an Input class to the main scene
 mainScene.input = new Input();
 
-
 // Establish update and draw loops
 const update = (delta) => {
-  mainScene.stepEntry(delta, mainScene)
+  mainScene.stepEntry(delta, mainScene);
 };
 const draw = () => {
-
   // Clear anything stale
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   // Draw the sky
-  skySprite.drawImage(ctx, 0, 0)
+  skySprite.drawImage(ctx, 0, 0);
 
   // Save the current state (for camera offset)
   ctx.save();
@@ -73,10 +71,13 @@ const draw = () => {
   ctx.restore();
 
   // Draw anything above the game world
-  inventory.draw(ctx, 0, 0)
+  inventory.draw(ctx, 0, 0);
+};
 
-}
+// const Game = () => {
+  // Start the game!
+  const gameLoop = new GameLoop(update, draw);
+  gameLoop.start();
+// };
 
-// Start the game!
-const gameLoop = new GameLoop(update, draw);
-gameLoop.start();
+// export default Game;
